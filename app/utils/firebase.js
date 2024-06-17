182708593c37982e89e3dca8ba0a1e3b089a157d
Adi1020 <129320700+Adi1020@users.noreply.github.com>
2024-06-18 00:55:08 +0300

    Update firebase.js

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -4,14 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   
-  apiKey: "AIzaSyAbHARgGEp_G5DOgCobrv0G0CAUtKy46d0",
-  authDomain: "check-it-out-265bd.firebaseapp.com",
-  projectId: "check-it-out-265bd",
-  storageBucket: "check-it-out-265bd.appspot.com",
-  messagingSenderId: "24869742191",
-  appId: "1:24869742191:web:c65b0eb1965899181bcf9a",
-  measurementId: "G-BD5TJ5XYS3",
-  databaseURL: "" 
+
 };
 
 const app = initializeApp(firebaseConfig);
