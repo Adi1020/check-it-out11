f2dcc2220421845aceb860ef02a2e546708926ec
Adi1020 <129320700+Adi1020@users.noreply.github.com>
2024-06-18 00:59:17 +0300

    Update firebase.js

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -3,7 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  
+    "apiKey": os.environ.get("API_KEY"),
+    "authDomain": os.environ.get("AUTH_DOMAIN"),
+    "projectId": os.environ.get("PROJECT_ID"),
+    "storageBucket": os.environ.get("STORAGE_BUCKET"),
+    "messagingSenderId": os.environ.get("MESSAGING_SENDER_ID"),
+    "appId": os.environ.get("APP_ID"),
+    "measurementId": os.environ.get("MEASUREMENT_ID"),
+    'databaseURL': os.environ.get("DATABASE_URL", ""),
 
 };
 
